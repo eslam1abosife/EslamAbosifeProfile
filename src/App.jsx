f0efae12cb0bd3519d622b38429d79e85fda6e6d
eslam abosife <eslamabosife69@gmail.com>
2026-08-09 09:08:02 +0300

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { Routes, Route } from 'react-router-dom'
-import { AnimatePresence } from 'framer-motion'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import Projects from './pages/Projects'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import InterviewQuestions from './pages/InterviewQuestions'  
-import { ThemeProvider } from './context/ThemeContext'
-import { LanguageProvider } from './context/LanguageContext'
+import { Routes, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import InterviewQuestions from "./pages/InterviewQuestions";
+import { ThemeProvider } from "./context/ThemeContext";
+import { LanguageProvider } from "./context/LanguageContext";
 
 function App() {
   return (
     <ThemeProvider>
       <LanguageProvider>
-        <div className="min-h-screen" style={{ background: 'var(--bg-primary)' }}>
+        <div
+          className="min-h-screen"
+          style={{ background: "var(--bg-primary)" }}
+        >
           <Navbar />
           <AnimatePresence mode="wait">
             <Routes>
@@ -22,14 +25,14 @@ function App() {
               <Route path="/projects" element={<Projects />} />
               <Route path="/about" element={<About />} />
               <Route path="/contact" element={<Contact />} />
-              <Route path="/questions" element={<InterviewQuestions />} /> 
+              <Route path="/questions" element={<InterviewQuestions />} />
             </Routes>
           </AnimatePresence>
           <Footer />
         </div>
       </LanguageProvider>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
